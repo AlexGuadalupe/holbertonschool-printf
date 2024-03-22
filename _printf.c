1edf61e9eba9d66a7702aac914d07a72c70a698a
Alex Guadalupe <9035@holbertonstudents.com>
2024-03-22 17:37:08 -0400

    fix printf add char counter

--- _printf.c
+++ _printf.c
@@ -9,23 +9,23 @@ int _printf(const char *format, ...)
 {
 int printed_chars = 0;
 va_list args;
-	va_start(args, format);
+va_start(args, format);
+
 while (*format != '\0')
 {
-
-    if (*format == '%')
-	{
-    format++;
-	printed_chars += print_parameters (*format, args);
-    }
+if (*format == '%')
+{
+format++;
+printed_chars++;
+printed_chars += print_parameters (*format, args);
+}
 else
 {
-    printed_chars += putchar(*format);
+putchar(*format);
+printed_chars++;
 }
-    format++;
+format++;
 }
-	va_end(args);
-
-	return (printed_chars);
-
+va_end(args);
+return (printed_chars);
 }
