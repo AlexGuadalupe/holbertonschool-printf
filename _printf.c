615761137aa7aeea7b252684469bb3b943eeffa0
Alex Guadalupe <9035@holbertonstudents.com>
2024-03-22 20:50:16 -0400

    fix code and added new files

--- _printf.c
+++ _printf.c
@@ -7,26 +7,12 @@
  */
 int _printf(const char *format, ...)
 {
-int printed_chars = 0;
-va_list args;
-va_start(args, format);
+	int printed_chars = 0;
+	va_list args;
 
-while (*format != '\0')
-{
-if (*format == '%')
-{
-format++;
-if (*format == '\0')
-break;
-printed_chars += print_parameters (*format, args);
-}
-else
-{
-putchar(*format);
-printed_chars++;
-}
-format++;
-}
-va_end(args);
-return (printed_chars);
+	va_start(args, format);
+	printed_chars = parse_format(format, args);
+	va_end(args);
+
+	return (printed_chars);
 }
